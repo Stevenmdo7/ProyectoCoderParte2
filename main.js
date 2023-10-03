35719f0b2320cfa1b84b95d47f9038a414b98fb4
Paolla Marcel Mascarello Montes de Oca <steven.montesdeoca.noc3g@gmail.com>
2023-10-03 15:45:36 -0300

    se modifica parte del codigo para hacer mas agradable visualmente y se agrega date en linea 7 a 9

--- main.js
+++ main.js
@@ -1,30 +1,29 @@
 const pacientes = []
-
 function agregarPaciente(nombre, apellido, edad, direccion, diagnostico) {
   if (!nombre || !apellido || isNaN(edad) || !direccion || !diagnostico) {
-    alert("Por favor, ingrese datos válidos para el paciente.")
+    alert("Por favor, ingrese datos validos para el paciente.")
     return false
   }
+  const fechaHoraIngreso = new Date()
+  const horaIngreso = `${fechaHoraIngreso.getHours()}:${fechaHoraIngreso.getMinutes()} ${fechaHoraIngreso.getHours() >= 12 ? 'PM' : 'AM'}`
+  const fechaIngreso = `${fechaHoraIngreso.getDate()}/${fechaHoraIngreso.getMonth() + 1}/${fechaHoraIngreso.getFullYear()}`
 
   pacientes.push({
     nombre,
     apellido,
     edad,
     direccion,
-    diagnostico
+    diagnostico,
+    fechaHoraIngreso: `${fechaIngreso} ${horaIngreso}`
   })
-
   return true
 }
-
 function eliminarPaciente() {
   let listaPacientes = "Lista de pacientes ingresados:\n"
   pacientes.forEach((paciente, index) => {
-    listaPacientes += `${index + 1}. ${paciente.nombre} ${paciente.apellido}\n`;
+    listaPacientes += `${index + 1}. ${paciente.nombre} ${paciente.apellido}\n`
   })
-
   const eliminator = prompt(listaPacientes + "\nIngrese el numero del paciente que desea eliminar:")
-
   if (isNaN(eliminator) || eliminator < 1 || eliminator > pacientes.length) {
     alert("Numero de paciente invalido.")
   } else {
@@ -32,20 +31,18 @@ function eliminarPaciente() {
     alert("Paciente eliminado con exito.")
   }
 }
-
 function mostrarPropiedades() {
-    let propiedadesDisponibles = "Propiedades disponibles para mostrar:\n" +
+  let propiedadesDisponibles = "Propiedades disponibles para mostrar:\n" +
     "1. Nombre\n" +
     "2. Apellido\n" +
     "3. Edad\n" +
-    "4. Dirección\n" +
-    "5. Diagnóstico\n"
+    "4. Direccion\n" +
+    "5. Diagnostico\n" +
+    "6. Fecha y Hora de Ingreso\n"
 
   const seleccion = prompt(propiedadesDisponibles + "\nIngrese el numero de la propiedad que desea ver (separe con comas para multiples propiedades):")
   const propiedadesSeleccionadas = seleccion.split(",").map(Number)
-
   let pacientesMostrados = "Pacientes:\n"
-
   pacientes.forEach((paciente) => {
     const propiedadesAMostrar = []
     propiedadesSeleccionadas.forEach((opcion) => {
@@ -65,42 +62,42 @@ function mostrarPropiedades() {
         case 5:
           propiedadesAMostrar.push(paciente.diagnostico)
           break
+        case 6:
+          propiedadesAMostrar.push(paciente.fechaHoraIngreso)
+          break
         default:
           break
       }
-    });
+    })
     pacientesMostrados += propiedadesAMostrar.join(", ") + "\n"
   })
 
   alert(pacientesMostrados)
 }
-
 agregarPaciente("Homero J.", "Simpson", 36, "Calle siempreviva 742", "Crayon en el cerebro")
-agregarPaciente("Peter", "Parker", 16, "69th Road, en Forest Hills", "Delirios de poderes por picadura de araña radioactiva")
+agregarPaciente("Peter", "Parker", 16, "69th Road, en Forest Hills", "Delirios de poderes por picadura de arana radioactiva")
 agregarPaciente("Bruce", "Wayne", 40, "Mansion Wayne", "quiroptofobia.")
-
 while (true) {
-  const opcion = prompt("Seleccione una opción:\n1. Agregar paciente\n2. Eliminar paciente\n3. Mostrar propiedades de pacientes\n4. Salir");
+  const opcion = prompt("Seleccione una opcion:\n1. Agregar paciente\n2. Eliminar paciente\n3. Mostrar propiedades de pacientes\n4. Salir")
 
   if (opcion === "1") {
     const nombre = prompt("Ingrese el nombre del paciente:")
     const apellido = prompt("Ingrese el apellido del paciente:")
     const edad = parseInt(prompt("Ingrese la edad del paciente:"))
-    const direccion = prompt("Ingrese la dirección del paciente:")
-    const diagnostico = prompt("Ingrese el diagnóstico del paciente:")
-    
-    const pacienteAgregado = agregarPaciente(nombre, apellido, edad, direccion, diagnostico);
-    
+    const direccion = prompt("Ingrese la direccion del paciente:")
+    const diagnostico = prompt("Ingrese el diagnostico del paciente:")
+    const pacienteAgregado = agregarPaciente(nombre, apellido, edad, direccion, diagnostico)
+
     if (pacienteAgregado) {
-      alert("Paciente agregado con éxito.");
+      alert("Paciente agregado con exito.")
     }
   } else if (opcion === "2") {
-    eliminarPaciente();
+    eliminarPaciente()
   } else if (opcion === "3") {
-    mostrarPropiedades();
+    mostrarPropiedades()
   } else if (opcion === "4") {
-    break;
+    break
   } else {
-    alert("Opción inválida. Por favor, seleccione una opción válida.")
+    alert("Opcion invalida. Por favor, seleccione una opcion valida.")
   }
 }
